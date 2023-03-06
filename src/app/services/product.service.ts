0911aeac63f312d50e5c145152446622ad3e1334
Emre Genç <emregenc696@gmail.com>
2023-03-06 19:03:50 +0300

    first commit

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { ListResponseModel } from '../models/listResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl = "https://localhost:44389/api/products/getall";
+
+  constructor(private httpClient:HttpClient) { }
+
+  getProducts(): Observable<ListResponseModel<Product>> {
+  return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl)
+    
+  }
+}
